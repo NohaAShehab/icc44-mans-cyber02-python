e5d5df59f965b9cb94a0ffaddb1d4bc9f9db9ee3
Noha Shehab <63148220+NohaAShehab@users.noreply.github.com>
2023-12-04 12:06:13 +0200

    loops and tips

---
+++ whileloop.py
@@ -0,0 +1,72 @@
+
+
+"""while loop ---> undefined number of iterations """
+
+# name = input("Enter your name: ")
+# while not name.isalpha():
+#     name = ("Not valid ,Enter your name again: ")
+#
+# print(name)
+
+""" deal with loops --- break , continue --->"""
+
+# for i in range(10):
+#     if i==5:
+#         break
+#
+#     print(f"i = {i}")
+
+
+# for i in range(10):
+#     if i==5:
+#         continue
+#
+#     print(f"i = {i}")
+
+# for i in range(3):
+#     password = input("please enter your password: ")
+#     if password=='abc':
+#         print("---logged in successfully---")
+#         break
+#
+#
+# if i==2:
+#     print("--- your account has been locked ")
+
+
+# for i in range(3):
+#     password = input("please enter your password: ")
+#     if password=='abc':
+#         print("---logged in successfully---")
+#         break
+# else:
+#     """ this block will be executed only when the loop reached its end  """
+#     print("--- your account has been locked ")
+
+
+# count= 0
+# while count <3:
+#     password = input("please enter your password: ")
+#     if password =='abc':
+#         print("----- login successful -----")
+#         break
+#     count +=1
+#
+#
+#
+# if count==3:
+#     print("---- your account has been locked -----")
+
+
+""" ---- 
+
+    if(name=='ahmed'){}
+
+"""
+name=True
+
+if name=='ahmed':
+    pass
+
+
+print("--- after if condition ---")
